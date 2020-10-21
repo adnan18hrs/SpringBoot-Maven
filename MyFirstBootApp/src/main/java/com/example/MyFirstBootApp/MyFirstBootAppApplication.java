8efec3f89c2b02f80ec01f3cf9b753da0a819f53
Khan, Md Adnan <adnan_khan@optum.com>
2020-10-21 23:02:53 +0530

    First time commited and pushed from new laptop

---
+++ MyFirstBootApp/src/main/java/com/example/MyFirstBootApp/MyFirstBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.MyFirstBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstBootAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(MyFirstBootAppApplication.class, args);
+	}
+
+}
