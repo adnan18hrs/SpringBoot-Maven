366880a17691e3c99ed14f05b9aa90260d0560f6
Khan, Md Adnan <adnan_khan@optum.com>
2020-10-21 23:10:08 +0530

    ..

--- MyFirstBootApp/src/main/java/com/example/MyFirstBootApp/MyFirstBootAppApplication.java
+++ MyFirstBootApp/src/main/java/com/example/MyFirstBootApp/MyFirstBootAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyFirstBootAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello_World");
 		SpringApplication.run(MyFirstBootAppApplication.class, args);
 	}
 
