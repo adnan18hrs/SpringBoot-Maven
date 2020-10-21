0492de5aa9d6b377b48408be01000495d022bb7d
Khan, Md Adnan <adnan_khan@optum.com>
2020-10-22 00:13:10 +0530

    simplechange

--- MyFirstBootApp/src/main/java/com/example/MyFirstBootApp/MyFirstBootAppApplication.java
+++ MyFirstBootApp/src/main/java/com/example/MyFirstBootApp/MyFirstBootAppApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyFirstBootAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello_World");
 		SpringApplication.run(MyFirstBootAppApplication.class, args);
+		System.out.println("Hello_World");
 	}
-
 }
